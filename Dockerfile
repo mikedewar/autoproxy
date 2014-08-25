18951754b9dc03c04128c589ca1f31d7d416ab32
Mike Dewar <mikedewar@gmail.com>
2014-08-25 15:08:59 +0000

    need to start nginx inside the watch script, not in the dockerfile

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,4 @@ RUN sudo apt-get install -y nginx curl
 ADD . .
 EXPOSE 80
 RUN chmod +x watch_etcd_autoproxy.sh
-RUN sudo service nginx start
 ENTRYPOINT /watch_etcd_autoproxy.sh
--- watch_etcd_autoproxy.sh
+++ watch_etcd_autoproxy.sh
@@ -1,9 +1,14 @@
+sudo service nginx start
 ./autoproxy
 nginx -s reload -c autoproxy.conf
 
+cat /usr/share/nginx/autoproxy.conf
+
 while :;
 do
-  curl -L "http://172.17.42.1:4001/v2/keys/endpoints?wait=true&recursive=true";
+  curl -L -s "http://172.17.42.1:4001/v2/keys/endpoints?wait=true&recursive=true";
   ./autoproxy
   nginx -s reload -c autoproxy.conf
+
+  cat /usr/share/nginx/autoproxy.conf
 done
